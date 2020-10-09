bafef46217c5537d9802bf318d6fc2421ef13066
SakthiDhandapani <sakthi.dhandapani@gmail.com>
2020-10-09 19:54:39 +0530

    Update LambdaFunctionHandler.java

--- src/main/java/com/amazonaws/lambda/demo/LambdaFunctionHandler.java
+++ src/main/java/com/amazonaws/lambda/demo/LambdaFunctionHandler.java
@@ -5,17 +5,19 @@ import java.time.LocalDateTime;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
-public class LambdaFunctionHandler implements RequestHandler<Object, String> {
-	
-	
-    @Override
-    public String handleRequest(Object input, Context context) {
-        context.getLogger().log("Input: " + input);
-        LocalDateTime date = LocalDateTime.now();
-        String name = "Haeron"+input+date;
-        return "HaeronSakthi@gmail.com!"+name;
-    }
-    
-   
-
+public class LambdaFunctionHandler implements RequestHandler<Map<String,String>, String>{
+  Gson gson = new GsonBuilder().setPrettyPrinting().create();
+  @Override
+  public String handleRequest(Map<String,String> event, Context context)
+  {
+    LambdaLogger logger = context.getLogger();
+    String response = new String("200 OK");
+    // log execution details
+    logger.log("ENVIRONMENT VARIABLES: " + gson.toJson(System.getenv()));
+    logger.log("CONTEXT: " + gson.toJson(context));
+    // process event
+    logger.log("EVENT: " + gson.toJson(event));
+    logger.log("EVENT TYPE: " + event.getClass().toString());
+    return response;
+  }
 }
