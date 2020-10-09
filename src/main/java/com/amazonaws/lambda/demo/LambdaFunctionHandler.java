25066e458c9f0220a72dfde7d3d33d1346d63ea7
SakthiDhandapani <sakthi.dhandapani@gmail.com>
2020-10-09 19:25:30 +0530

    Add files via upload

---
+++ src/main/java/com/amazonaws/lambda/demo/LambdaFunctionHandler.java
@@ -0,0 +1,21 @@
+package com.amazonaws.lambda.demo;
+
+import java.time.LocalDateTime;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+public class LambdaFunctionHandler implements RequestHandler<Object, String> {
+	
+	
+    @Override
+    public String handleRequest(Object input, Context context) {
+        context.getLogger().log("Input: " + input);
+        LocalDateTime date = LocalDateTime.now();
+        String name = "Haeron"+input+date;
+        return "Hello from Lambda!"+name;
+    }
+    
+   
+
+}
